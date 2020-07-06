24bd5890821d65bd8057fb950dee6ea7d3c1f785
Crisbern <tumangcris@gmail.com>
2020-07-06 23:31:08 +0800

    Lower case problem fixed

--- generator/views.py
+++ generator/views.py
@@ -7,7 +7,8 @@ def home(request):
     return render(request, 'generator/home.html')
 
 def password(request):
-    passwordCharcters = list('abcdefghijklmnopqrstuvwxyz')
+    passwordCharcters = list('')
+    isEmpty = True
 
     try:
         length = int(request.GET.get('length'))
@@ -18,12 +19,22 @@ def password(request):
 
     if request.GET.get('uppercase'):
         passwordCharcters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+        isEmpty = False
+
+    if request.GET.get('lowercase'):
+        passwordCharcters.extend(list('abcdefghijklmnopqrstuvwxyz'))
+        isEmpty = False
 
     if request.GET.get('special'):
         passwordCharcters.extend(list('~!@#$%^&*()_+?'))
+        isEmpty = False
 
     if request.GET.get('numbers'):
-        passwordCharcters.extend(list('1234567890'))        
+        passwordCharcters.extend(list('1234567890'))
+        isEmpty = False
+
+    if isEmpty:
+        passwordCharcters.extend(list('abcdefghijklmnopqrstuvwxyz'))       
 
     for characters in range(length):
         thePassword += random.choice(passwordCharcters)
